3531b6965acecc629600c42d7814675fe4a1ed00
Alex Sidorenko <my.mail.kharkov@gmail.com>
2018-06-23 22:09:00 +0100

    Remove example video

--- example/src/App.js
+++ example/src/App.js
@@ -3,37 +3,15 @@ import { AzureMP } from 'react-azure-mp'
 import styles from './index.css'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      src: [
-        {
-          src:
-            'https://pobble.streaming.mediaservices.windows.net/d5aa7dd9-edd6-4c04-8560-6c5bf5f06460/0 Introduction - DRAFT.ism/manifest',
-          type: 'application/vnd.ms-sstr+xml'
-        }
-      ]
-    }
-  }
   doSomething(player) {
-    player.addEventListener('ended', () =>
-      this.setState({
-        src: [
-          {
-            src:
-              'http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest',
-            type: 'application/vnd.ms-sstr+xml'
-          }
-        ]
-      })
-    )
+    this.player = player
   }
   render() {
     return (
       <div className="container">
         <AzureMP
-      skin="amp-flush"
-      src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
+          skin="amp-flush"
+          src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
       </div>
     )
   }
