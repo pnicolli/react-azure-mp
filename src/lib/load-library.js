6c57e6e29deaa286c340b3037548a604d51b290c
SidKH <my.mail.kharkov@gmail.com>
2020-07-29 00:50:57 +0300

    Update azure library

--- src/lib/load-library.js
+++ src/lib/load-library.js
@@ -5,8 +5,8 @@ export default (skin = 'amp-default') => {
     let linkTag = document.createElement('link')
     linkTag.rel = 'stylesheet'
     scriptTag.id = 'amp-azure'
-    scriptTag.src = '//amp.azure.net/libs/amp/2.1.5/azuremediaplayer.min.js'
-    linkTag.href = `//amp.azure.net/libs/amp/2.1.5/skins/${skin}/azuremediaplayer.min.css`
+    scriptTag.src = '//amp.azure.net/libs/amp/2.3.5/azuremediaplayer.min.js'
+    linkTag.href = `//amp.azure.net/libs/amp/2.3.5/skins/${skin}/azuremediaplayer.min.css`
     document.body.appendChild(scriptTag)
     document.head.insertBefore(linkTag, document.head.firstChild)
     scriptTag.onload = () => resolve({ skin: skin })
