62f9eff2c2193d2092aced8328f644c14546872d
SidKH <my.mail.kharkov@gmail.com>
2020-12-05 14:47:13 +0000

    Fix multi instance bug

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { AzureMP } from 'react-azure-mp'
-import styles from './index.css'
 
 export default class App extends Component {
   doSomething(player) {
@@ -9,6 +8,12 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
+        <AzureMP
+          skin="amp-flush"
+          src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
+        <AzureMP
+          skin="amp-flush"
+          src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
         <AzureMP
           skin="amp-flush"
           src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
--- src/lib/load-library.js
+++ src/lib/load-library.js
@@ -1,14 +1,18 @@
+let loader = false
+
 export default (skin = 'amp-default') => {
-  return new Promise((resolve, reject) => {
-    if (document.querySelector('#amp-azure')) return resolve()
-    let scriptTag = document.createElement('script')
-    let linkTag = document.createElement('link')
-    linkTag.rel = 'stylesheet'
-    scriptTag.id = 'amp-azure'
-    scriptTag.src = '//amp.azure.net/libs/amp/2.3.5/azuremediaplayer.min.js'
-    linkTag.href = `//amp.azure.net/libs/amp/2.3.5/skins/${skin}/azuremediaplayer.min.css`
-    document.body.appendChild(scriptTag)
-    document.head.insertBefore(linkTag, document.head.firstChild)
-    scriptTag.onload = () => resolve({ skin: skin })
-  })
+  if (!loader) {
+    loader = new Promise((resolve, reject) => {
+      let scriptTag = document.createElement('script')
+      let linkTag = document.createElement('link')
+      linkTag.rel = 'stylesheet'
+      scriptTag.id = 'amp-azure'
+      scriptTag.src = '//amp.azure.net/libs/amp/2.3.5/azuremediaplayer.min.js'
+      linkTag.href = `//amp.azure.net/libs/amp/2.3.5/skins/${skin}/azuremediaplayer.min.css`
+      document.body.appendChild(scriptTag)
+      document.head.insertBefore(linkTag, document.head.firstChild)
+      scriptTag.onload = () => resolve({ skin: skin })
+    })
+  }
+  return loader
 }
