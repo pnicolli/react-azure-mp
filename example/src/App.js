b1061721d326dc0d8f2d8f646886e8f0a613b4e1
SidKH <my.mail.kharkov@gmail.com>
2021-03-18 18:20:56 +0300

    Update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
+import React, { useMemo, useState } from 'react'
 import { AzureMP } from 'react-azure-mp'
 
-export default class App extends Component {
-  doSomething(player) {
-    this.player = player
-  }
-  render() {
-    return (
+const App = () => {
+  const [counter, setCounter] = useState(1)
+  const src = useMemo(() => [{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }], [])
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Click</button>
       <div className="container">
         <AzureMP
           skin="amp-flush"
-          src={[{src: "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest", type: "application/vnd.ms-sstr+xml" }]} />
+          src={src} />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
